ca0ef52abf4723a806fdb39ada35888a4aaa9fd8
mathanpandi.p <mathanpandi.paramasivan@ideas2it.com>
2025-04-09 19:49:14 +0530

    Pipes examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CounterButtonsComponent } from './components/counter-buttons/counter-bu
 import { ValueDisplayComponent } from './components/value-display/value-display.component'; // Import the new component
 import { ListProductComponent } from './components/list-product/list-product.component'; // Import the new component
 import { HighlightDirective } from './directives/HighlightDirective'; // Import the new component
+import {ReversePipe} from './pipes/ReversePipe'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true, 
-  imports: [ValueDisplayComponent, CounterButtonsComponent, ListProductComponent, HighlightDirective],
+  imports: [ValueDisplayComponent, CounterButtonsComponent, ListProductComponent, HighlightDirective, ReversePipe],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
